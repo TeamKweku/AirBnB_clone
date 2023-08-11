42885f4e22264878dd059b1fed3d1697d1014b18
victormayowa <hardmanvicky01@gmail.com>
2023-08-11 16:10:22 -0700

    create the city class

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for City class.
+"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    City class has BaseModel as parent.
+    """
+    state_id = ""
+    name = ""
+
