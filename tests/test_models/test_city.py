ff4c688acc88f1d3558b94663361e77e2889ba18
victormayowa <hardmanvicky01@gmail.com>
2023-08-13 05:46:48 -0700

    test city

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''
+Module to test the city module in base model library
+'''
+
+import unittest
+from models.city import City
+from datetime import datetime
+
+
+class TestCity(unittest.TestCase):
+    def test_instance(self):
+        city1 = City()
+        city2 = City()
+        self.assertIsInstance(city1, City)
+        self.assertIsInstance(city2, City)
+
+    def test_attributes(self):
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        self.assertTrue(hasattr(city, "name"))
+
+    def test_state_id_attribute(self):
+        city = City()
+        self.assertEqual(city.state_id, "")
+
+    def test_name_attribute(self):
+        city = City()
+        self.assertEqual(city.name, "")
+
+    def test_id_type(self):
+        city = City()
+        self.assertIsInstance(city.id, str)
+
+    def test_created_at_type(self):
+        city = City()
+        self.assertIsInstance(city.created_at, datetime)
+
+    def test_updated_at_type(self):
+        city = City()
+        self.assertIsInstance(city.updated_at, datetime)
+
+    def test_to_dict(self):
+        city = City()
+        city_dict = city.to_dict()
+        self.assertIsInstance(city_dict, dict)
+        self.assertIn("state_id", city_dict)
+        self.assertIn("name", city_dict)
+        self.assertIn("__class__", city_dict)
+        self.assertIn("id", city_dict)
+        self.assertIn("created_at", city_dict)
+        self.assertIn("updated_at", city_dict)
